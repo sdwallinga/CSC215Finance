4779069005a263e50f8862c37b552dabf0454927
sdw <samwallinga@gmail.com>
2014-01-23 06:19:33 -0600

    all cases handled for calculator, bug fixes

--- js/calculator.js
+++ js/calculator.js
@@ -3,12 +3,13 @@
 // get a list of all span tags in the calculator div
 var keys = document.querySelectorAll('#calculator span');
 var hasDecimal = false;
-var lastOp = false;
 var re = /[\+\-\*\/]$/;
 
 // assign event handlers to all keys
 for (var i = 0; i < keys.length; i++) {
   keys[i].onclick = function(e) {
+
+    // Get the value of the key pressed
     var keyVal = this.innerHTML;
 
     //when CLEAR is pressed, reset the display
@@ -18,22 +19,33 @@ for (var i = 0; i < keys.length; i++) {
     }
     // when eval is pressed, evaluate the expression
     else if(keyVal == '=') {
-      document.getElementById('screen').innerHTML = eval(document.getElementById('screen').innerHTML);
+      // Users shall not evaluate empty expressions! Handle!
+      if (document.getElementById('screen').innerHTML == '') {
+        nullAppend();
+      }
+      else {
+        document.getElementById('screen').innerHTML = eval(document.getElementById('screen').innerHTML);
+      }
     }
     // otherwise, append the number or expression
     else {
-      // Users shall not add two decimal points! Handle.
+      // Users shall not add two decimal points! Handle!
       if(keyVal == '.' && hasDecimal == false){
         hasDecimal = true;
         document.getElementById('screen').innerHTML += keyVal;
       }
       else if(keyVal == '.' && hasDecimal == true) {
-        document.getElementById('screen').innerHTML += '';
+        nullAppend();
+      }
+      // Users shall not impose an operand upon a null equation! Handle!
+      else if((document.getElementById('screen').innerHTML) == '' && (re.test(keyVal))) {
+        // Turning numbers negative, however, is permitted.
+        ((keyVal == '-')? document.getElementById('screen').innerHTML += keyVal : nullAppend());
       }
       // Users shall not add two operands consecutively! Handle! (warning: ugly)
       else if(re.test(document.getElementById('screen').innerHTML)) {
         if(re.test(keyVal)) {
-          document.getElementById('screen').innerHTML += '';
+          nullAppend();
         }
         else {
           document.getElementById('screen').innerHTML += keyVal;
@@ -44,5 +56,11 @@ for (var i = 0; i < keys.length; i++) {
       }
     }
   }
+}
+
+
+// Helper function to save typing. (can't get the context-sensitive actual append() to work just yet...)
+function nullAppend() {
+  document.getElementById('screen').innerHTML += '';
 }
 
